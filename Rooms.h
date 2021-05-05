7038b093adcdcbe7547f19ba93c93157f458e506
Fijona999 <80392331+Fijona999@users.noreply.github.com>
2021-05-05 16:05:01 +0300

    Update Rooms.h

--- Rooms.h
+++ Rooms.h
@@ -1,1 +1,35 @@
+#ifndef ROOMS_H
+#define ROOMS_H
+#include <stdexcept>
 
+namespace gl{
+           class Rooms {
+               public:
+                     string colour;
+                     vector<string> Rooms;
+		     int ID;
+                     Rooms()
+                     ~Rooms()
+                     void setColour(string input)
+                     string getColour() 
+                     void addRooms(string input)
+                     void printRooms()
+  
+                     bool operator==(const  Rooms&) const;
+                     bool operator!=(const  Rooms&) const;
+                     bool operator>(const  Rooms&) const;
+                     bool operator<(const  Rooms&) const;
+                     bool operator>=(const  Rooms&) const;
+                     bool operator<=(const  Rooms&) const; 
+             
+                     Rooms operator++();
+                     Rooms operator++(int);
+                     Rooms operator--();
+                     Rooms operator--(int);
+             
+                     friend ostream &operator<<(ostream&, const Rooms&);
+                     friend istream &operator>>(istream&, Rooms&);
+	  }
+  };
+	
+#endif
