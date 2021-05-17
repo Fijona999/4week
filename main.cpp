9d41f9c9f8a9d02fb4a2c56d8e081dd2d5be70f9
Fijona999 <80392331+Fijona999@users.noreply.github.com>
2021-05-17 13:10:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <time.h>
+#include "Cube.hpp"
 
+using namespace std;
+
+
+
+int main(){
+
+try{
+
+    Rooms r1;
+    cout << "Room Color and Rooms" << endl;
+    cin >> r1;
+    cout << r1 << endl;
+
+    cout << "Compare:" << endl;
+    Rooms r2;
+    r2.setColour("Red");
+    string  compare = r1 == r2;
+    cout << compare << " " <<endl;
+
+    r1++;
+    cout << r1 << endl;
+
+    --r1;
+    cout << r1 << endl;
+
+}
+
+catch(invalid_argumant &error)
+{ 
+  cout <<error.what() << endl;
+}
+catch ( ... )
+{
+  cout << " unknown happened" << endl;
+}
+return 0;
+}
